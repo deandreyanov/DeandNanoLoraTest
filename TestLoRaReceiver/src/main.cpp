8774558d30e293904be60400f30b9c42a8a0e30e
andreyanov <andreyanov@prima.local>
2019-11-13 21:40:56 +0300

    add send and receive

--- TestLoRaReceiver/src/main.cpp
+++ TestLoRaReceiver/src/main.cpp
@@ -1,32 +1,59 @@
 #include <SPI.h>
 #include <LoRa.h>
 
-void setup() {
+float Temperature = 0;
+int32_t Pressure = 0;
+float Altitude = 0;
+int counter = 0;
+int prev_counter = 0;
+
+void setup()
+{
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial)
+    ;
 
   Serial.println("LoRa Receiver");
 
-  if (!LoRa.begin(433E6)) {
+  if (!LoRa.begin(433E6))
+  {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1)
+      ;
   }
 }
 
-void loop() {
+void loop()
+{
   // try to parse packet
   int packetSize = LoRa.parsePacket();
-  if (packetSize) {
+  if (packetSize)
+  {
     // received a packet
-    Serial.print("Received packet '");
+    Serial.print("Received packet '");    
+    Temperature = LoRa.read();
+    Pressure = LoRa.read();
+    Altitude = LoRa.read();
+    counter = LoRa.read();
 
-    // read packet
-    while (LoRa.available()) {
-      Serial.print((char)LoRa.read());
-    }
+    Serial.print("Temperature = ");
+    Serial.print(Temperature);
+    Serial.println(" *C");
+
+    Serial.print("Pressure = ");
+    Serial.print(Pressure);
+    Serial.println(" Pa");
+
+    // Calculate altitude assuming 'standard' barometric
+    // pressure of 1013.25 millibar = 101325 Pascal
+    Serial.print("Altitude = ");
+    Serial.print(Altitude);
+    Serial.println(" meters");
 
     // print RSSI of packet
     Serial.print("' with RSSI ");
     Serial.println(LoRa.packetRssi());
+    Serial.print("counter = ");
+    Serial.println(counter);
   }
 }
--- TestLoRaSender/src/main.cpp
+++ TestLoRaSender/src/main.cpp
@@ -1,31 +1,63 @@
 #include <SPI.h>
 #include <LoRa.h>
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
 
+float Temperature = 0;
+int32_t Pressure = 0;
+float Altitude = 0;
 int counter = 0;
 
-void setup() {
+Adafruit_BMP085 bmp;
+
+void setup()
+{
   Serial.begin(9600);
-  while (!Serial);
+  while (!Serial){}
 
   Serial.println("LoRa Sender");
 
-  if (!LoRa.begin(433E6)) {
+  if (!LoRa.begin(433E6))
+  {
     Serial.println("Starting LoRa failed!");
-    while (1);
+    while (1){}
+  }
+
+  if (!bmp.begin()) 
+  {
+    Serial.println("Could not find a valid BMP085 sensor, check wiring!");
+    while (1) {}
   }
 }
 
-void loop() {
+void loop()
+{
+  Temperature = bmp.readTemperature();
+  Pressure = bmp.readPressure();
+  Altitude = bmp.readAltitude();
+
   Serial.print("Sending packet: ");
   Serial.println(counter);
+  
+  Serial.print("Temperature = ");
+  Serial.print(Temperature);
+  Serial.println(" *C");
 
-  // send packet
+  Serial.print("Pressure = ");
+  Serial.print(Pressure);
+  Serial.println(" Pa");
+  
+  Serial.print("Altitude = ");
+  Serial.print(Altitude);
+  Serial.println(" meters");
+  Serial.println();
+  
   LoRa.beginPacket();
-  LoRa.print("hello ");
-  LoRa.print(counter);
+  LoRa.write(Temperature);
+  LoRa.write(Pressure);
+  LoRa.write(Altitude);
+  LoRa.write(counter);
   LoRa.endPacket();
-
   counter++;
-
   delay(5000);
 }
