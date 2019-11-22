19b1df08f7cc13a4b088aad7626477427a0bb4ba
Deand <deandreyanov@gmail.com>
2019-11-22 08:43:11 +0300

    add oled display

--- TestLoRaReceiver/src/main.cpp
+++ TestLoRaReceiver/src/main.cpp
@@ -1,5 +1,15 @@
 #include <SPI.h>
 #include <LoRa.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET 4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 union float2uint8_t {
   float fVal;
@@ -28,6 +38,21 @@ void setup()
     while (1)
       ;
   }
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+  { // Address 0x3C for 128x32
+    Serial.println(F("SSD1306 allocation failed"));
+    //for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
 }
 
 void loop()
@@ -36,6 +61,7 @@ void loop()
   int packetSize = LoRa.parsePacket();
   if (packetSize)
   {
+    display.clearDisplay();
     // received a packet
     Serial.print("Received packet '");
     //    Temperature = LoRa.read();
@@ -68,7 +94,7 @@ void loop()
     my_float2uint8_t.buf[1] = LoRa.read();
     my_float2uint8_t.buf[2] = LoRa.read();
     my_float2uint8_t.buf[3] = LoRa.read();
-    counter = my_float2uint8_t.fVal;    
+    counter = my_float2uint8_t.fVal;
 
     Serial.print("Temperature = ");
     Serial.print(Temperature);
@@ -93,5 +119,33 @@ void loop()
     Serial.println(LoRa.packetRssi());
     Serial.print("counter = ");
     Serial.println(counter);
+
+    display.setTextSize(1);              // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE); // Draw white text
+    display.setCursor(0, 0);             // Start at top-left corner
+
+    display.print(F("T="));
+    display.print(Temperature);
+    display.print("*C|");
+
+    display.print(F("p="));
+    display.print(Pressure);
+    display.print(" Pa");
+
+    display.print(F("H="));
+    display.print(Altitude);
+    display.println(" m");
+
+    display.print(F("V="));
+    display.print(voltage);
+    display.println(" mV");
+
+    display.print(F("Cnt="));
+    display.print(counter);
+
+    display.print(F("|Time="));
+    display.println(millis());
+
+    display.display();
   }
 }
